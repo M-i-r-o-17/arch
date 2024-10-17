98b494afc765736e22f7004dbf35fa888fb0bc2f
m_i_r_o_17 <m_i_r_o_17@vk.com>
2024-10-18 08:19:22 +0900

    Add new generate wallpaper

--- hypr/random_wrapper.sh
+++ hypr/random_wrapper.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 name=$(shuf -i 1-372 -n 1)
-#rm $HOME/.config/hypr/hyprpaper.conf
 
 wallpaper=$(find $HOME/Изображения/Wallpaper -type f -name "$name.*" -print)
-echo $wallpaper
 
 echo "preload = $wallpaper" > $HOME/.config/hypr/hyprpaper.conf
 echo "" >> $HOME/.config/hypr/hyprpaper.conf
