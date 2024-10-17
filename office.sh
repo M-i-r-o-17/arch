4f6cb5acd510adde706f69e6fbe0e5fe056c7311
Vladimir <69893445+M-i-r-o-17@users.noreply.github.com>
2024-10-17 11:39:54 +0900

    Add files via upload

---
+++ office.sh
@@ -0,0 +1,5 @@
+yay -S ttf-ms-fonts
+yay -S ttf-hack-nerd
+sudo pacman -S noto-fonts-cjk
+sudo pacman -S aspell aspell-en aspell-ru
+yay -S onlyoffice-bin
---
+++ printer_instal.sh
@@ -0,0 +1,4 @@
+sudo pacman -S cups gutenprint gtk3-print-backends
+yay pantum-driver
+sudo systemctl enable cups
+sudo pacman -S ghostpcl ghostxps
