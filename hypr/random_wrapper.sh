ed64bbc0d165583f3ca14f00611088908fb9c93c
Vladimir <69893445+M-i-r-o-17@users.noreply.github.com>
2024-10-17 15:20:24 +0900

    100% коректная работа проги для изменения обоев

--- hypr/random_wrapper.sh
+++ hypr/random_wrapper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+name=$(shuf -i 1-372 -n 1)
+#rm $HOME/.config/hypr/hyprpaper.conf
 
-wrapper="$(shuf -i 1-372 -n 1).jpg"
-rm $HOME/.config/hypr/hyprpaper.conf
+wallpaper=$(find $HOME/Изображения/Wallpaper -type f -name "$name.*" -print)
+echo $wallpaper
 
-echo "preload = $HOME/Images/$wrapper" > $HOME/.config/hypr/hyprpaper.conf
+echo "preload = $wallpaper" > $HOME/.config/hypr/hyprpaper.conf
 echo "" >> $HOME/.config/hypr/hyprpaper.conf
-echo "wallpaper = eDP-1,$HOME/Images/$wrapper" >> $HOME/.config/hypr/hyprpaper.conf
+echo "wallpaper = eDP-1,$wallpaper" >> $HOME/.config/hypr/hyprpaper.conf
 
 killall hyprpaper
 exec hyprpaper &
